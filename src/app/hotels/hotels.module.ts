da7f2512343e4d2e8bf77ec1f2e2ebf821584554
shady-abdelhamid <shady.abdelhamid@outlook.com>
2020-02-18 20:47:13 +0200

    Update hotels module
    1.  add a service for hotels.
    2. use BE request instead of MOCK data.
    3. use Observable for async.
    4. use a baseUrl.
    5. add currency pipe.

--- src/app/hotels/hotels.module.ts
+++ src/app/hotels/hotels.module.ts
@@ -6,14 +6,20 @@ import { HotelsComponent } from './hotels.component';
 import { HotelListingComponent } from './hotel-listing/hotel-listing.component';
 import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { HttpClientModule } from '@angular/common/http';
+import { HotelsService } from './hotels.service';
 
 @NgModule({
   declarations: [HotelsComponent, HotelListingComponent],
   imports: [
     CommonModule,
     HotelsRoutingModule,
+    HttpClientModule,
     NgbAccordionModule,
     FontAwesomeModule,
+  ],
+  providers: [
+    HotelsService
   ]
 })
 export class HotelsModule { }
---
+++ src/app/hotels/hotels.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Hotel } from './hotel.model';
+import { Observable, throwError } from 'rxjs';
+import { tap, catchError, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HotelsService {
+  baseUrl = environment.baseUrl;
+  constructor(private http: HttpClient) { }
+
+  getHotels(): Observable<Hotel[]> {
+    return this.http.get<Hotel[]>(`${this.baseUrl}/tl0bp`).pipe(
+        tap(_ => console.log('fetched hotels')),
+        map(res => res['hotels']),
+        catchError(err => throwError(err))
+      );
+  }
+}
