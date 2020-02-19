0155ed44d4cfb323a1e0a24de8e4757f610f0365
shady-abdelhamid <shady.abdelhamid@outlook.com>
2020-02-19 11:47:26 +0200

    add search hotel logic

--- src/app/hotels/hotel-listing/hotel-listing.component.ts
+++ src/app/hotels/hotel-listing/hotel-listing.component.ts
@@ -1,23 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import { Hotel } from '../hotel.model';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { fromEvent, Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, startWith, switchMap } from 'rxjs/operators';
+import { Hotel } from '../hotel.model';
 import { HotelsService } from '../hotels.service';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'ft-hotel-listing',
   templateUrl: './hotel-listing.component.html',
   styleUrls: ['./hotel-listing.component.scss']
 })
-export class HotelListingComponent implements OnInit {
+export class HotelListingComponent implements OnInit, AfterViewInit {
   faSearch = faSearch;
-  
+
   hotels$: Observable<Hotel[]>;
+
+  @ViewChild('searchInput') searchInput: ElementRef;
 
   constructor(private hotelsService: HotelsService) { }
 
-  ngOnInit(): void {
-    this.hotels$ = this.hotelsService.getHotels();
+  ngOnInit(): void {}
+
+  /**
+     * display hotels according to search term. 
+     * if not display all hotels
+     */
+  ngAfterViewInit() {
+    this.hotels$ = fromEvent(this.searchInput.nativeElement, 'keyup')
+      .pipe(
+        map(event => event['target'].value),
+        startWith(''),
+        debounceTime(400),
+        distinctUntilChanged(),
+        switchMap(search => this.loadHotels(search))
+      );
+
+
+  }
+
+  /** loads hotels according the search term
+   * @param hotelSearchTerm a string with defaults value of empty
+   */
+  loadHotels(hotelSearchTerm = ''): Observable<Hotel[]> {
+    return this.hotelsService.getHotels().pipe(
+      map(hotels => hotels
+        .filter(h => h.name.trim().toLowerCase()
+          .search(hotelSearchTerm.toLowerCase()) >= 0
+        )));
   }
 
 }
