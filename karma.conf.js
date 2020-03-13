b7051f1b06349bfdefda2082a580f1def6b4f79d
shady-abdelhamid <shady.abdelhamid@outlook.com>
2020-03-13 21:18:32 +0200

    update test conf

--- karma.conf.js
+++ karma.conf.js
@@ -19,19 +19,18 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, './coverage/Front-End-Task'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
-      thresholds: {
-        statements: 10,
-        lines: 10,
-        branches: 10,
-        functions: 10
-      }
+      // thresholds: {
+      //   statements: 10,
+      //   lines: 10,
+      //   branches: 10,
+      //   functions: 10
+      // }
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
     browsers: ['Chrome'],
     customLaunchers: {
       ChromeHeadlessCI: {
