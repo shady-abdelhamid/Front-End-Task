295a2e63f4f53b523dbb91426345d877af598aa4
shady-abdelhamid <shady.abdelhamid@outlook.com>
2020-03-13 09:16:45 +0200

    update test config

--- karma.conf.js
+++ karma.conf.js
@@ -31,13 +31,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
-    customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
-    },
+    browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
   });
